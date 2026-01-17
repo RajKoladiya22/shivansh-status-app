0c1398aac61e6c1eaf72ba77f10e02a5fb9def3d
koladiyaraj22 <koladiyaraj22@gmail.com>
2026-01-17 15:18:34 +0530

    lead

--- app/lead-assign/page.tsx
+++ app/lead-assign/page.tsx
@@ -3,7 +3,31 @@ import React, { useState } from "react";
 import { AutocompleteInput, Header } from "@/component";
 import { Copy, Check, Send } from "lucide-react";
 
+type Lead = {
+  leadTypes: string; // ["2 Tally", "3 WhatsApp"]
+  customerName: string;
+  mobile: string;
+  products: string[]; // multi-select
+  cost: number;
+  assignedTo: string;
+  status: string;
+  remark: string;
+};
+
+const LEAD_TYPES = ["Tally", "WhatsApp", "Cloud", "BizAnalyst", "TDL Lead"];
+
 export default function LeadAssignPage() {
+  const [lead, setLead] = useState<Lead>({
+    leadTypes: "",
+    customerName: "",
+    mobile: "",
+    products: [],
+    cost: 0,
+    assignedTo: "",
+    status: "",
+    remark: "",
+  });
+
   const [adminName, setAdminName] = useState("Mehul Patel");
 
   const [customerName, setCustomerName] = useState("");
@@ -30,13 +54,15 @@ export default function LeadAssignPage() {
 
   function buildMessage() {
     return (
-      `*Lead*\n\n` +
+      // `*Lead*\n\n` +
+      `*${lead.leadTypes} - Lead*\n\n` +
       `*Customer Name:* ${customerName || "—"}\n` +
       `*Mobile Number:* ${mobile || "—"}\n` +
-      `*Product:* ${product || "—"}\n` +
+      `${product ? `*Product:* ${product || "—"}\n` : ""}` +
       `*Cost:* ${cost || "—"}\n` +
       `*Assigned to:* ${assignedTo || "—"}\n` +
-      `*Remark:* ${remark || "—"}\n\n` +
+      `*Status* ${lead.status || "—"}\n` +
+      `${remark ? `*Remark:* ${remark || "—"}\n\n` : ""}` +
       `*By* - ${adminName}\n`
     );
   }
@@ -68,6 +94,45 @@ export default function LeadAssignPage() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           {/* LEFT: FORM */}
           <div className="bg-white rounded-lg shadow-sm p-5 space-y-5">
+            {/* <div className="grid grid-cols-2 gap-2">
+              {LEAD_TYPES.map((t) => (
+                <label key={t} className="flex items-center gap-2 text-sm">
+                  <input
+                    type="checkbox"
+                    checked={lead.leadTypes.includes(t)}
+                    onChange={(e) =>
+                      setLead((prev) => ({
+                        ...prev,
+                        leadTypes: e.target.value,
+                      }))
+                      
+                    }
+                  />
+                  {t}
+                </label>
+              ))}
+            </div> */}
+            <label className="block text-xs font-semibold text-gray-700 mb-1">
+              Lead for *
+            </label>
+            <select
+              className="w-full px-3 py-2.5 border-2 border-gray-300 rounded-lg text-sm outline-none bg-white focus:ring-2 focus:ring-blue-500"
+              value={lead.leadTypes}
+              onChange={(e) =>
+                setLead((prev) => ({
+                  ...prev,
+                  leadTypes: e.target.value,
+                }))
+              }
+            >
+              <option value="">Select Lead Type</option>
+              {LEAD_TYPES.map((t) => (
+                <option key={t} value={t}>
+                  {t}
+                </option>
+              ))}
+            </select>
+
             {/* Admin Name */}
             <div>
               <label className="block text-sm font-bold text-gray-900 mb-1">
@@ -153,6 +218,21 @@ export default function LeadAssignPage() {
                 </select>
               </div>
 
+              <label className="block text-xs font-semibold text-gray-700 mb-1">
+                Status *
+              </label>
+
+              <select
+                value={lead.status}
+                onChange={(e) => setLead({ ...lead, status: e.target.value })}
+                className="w-full px-3 py-2.5 border-2 border-gray-300 rounded-lg text-sm outline-none bg-white focus:ring-2 focus:ring-blue-500"
+              >
+                <option value="">Select Status</option>
+                <option value="Pending">Pending</option>
+                <option value="Done">Done</option>
+                <option value="Follow-up">Follow-up</option>
+              </select>
+
               <div>
                 <label className="block text-xs font-semibold text-gray-700 mb-1">
                   Remark
@@ -196,22 +276,18 @@ export default function LeadAssignPage() {
                 {buildMessage()}
               </pre>
             </div>
-                          <button
-                onClick={sendToWhatsApp}
-                className="absolute bottom-0 right-5 z-50 flex items-center gap-2 
+            <button
+              onClick={sendToWhatsApp}
+              className="absolute bottom-0 right-5 z-50 flex items-center gap-2 
              px-3 py-2 bg-green-700 text-white rounded-full 
              hover:opacity-90 active:scale-95 transition-all 
              text-sm font-bold shadow-lg mb-2"
-              >
-                <svg
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.890-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
-                </svg>
-                WhatsApp
-              </button>
+            >
+              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.890-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
+              </svg>
+              WhatsApp
+            </button>
           </div>
         </div>
       </div>
